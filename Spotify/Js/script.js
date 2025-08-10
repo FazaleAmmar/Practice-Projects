2e48c53cd3de1f8bb2558ed306a982a043cb7881
FazaleAmmar <fazaleammar786@gmail.com>
2025-08-10 11:59:10 +0500

    Some progress

--- Spotify/Js/script.js
+++ Spotify/Js/script.js
@@ -1,11 +1,37 @@
-async function main() {
+async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
   let response = await a.text();
-  console.log(response);
   let div = document.createElement("div");
   div.innerHTML = response;
-  let tds = div.getElementsByTagName("td");
-  console.log(tds);
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split(/songs/)[1]);
+    }
+  }
+  return songs;
+}
+
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
+
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML + `<li> ${song.replaceAll("/", "")} </li>`;
+  }
+
+  var audio = new Audio(songs[0]);
+  audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    console.log(audio.duration, audio.currentTime, audio.currentSrc);
+  });
 }
 
 main();
