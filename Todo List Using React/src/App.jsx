75072321583fb0ba02a125cf99cfa787f93b35b7
FazaleAmmar <fazaleammar786@gmail.com>
2025-11-27 18:28:15 +0500

    Initialization of the todo list using react

---
+++ Todo List Using React/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-white">
+        This is Todex.
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ Todo List Using React/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="img h-30 w-full flex items-center justify-center bg-amber-50">
+        <a href="/">
+          <img
+            className="h-25 w-80 object-center object-cover"
+            src="/public/Todex_logo.png"
+            alt="todex logo"
+          />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
