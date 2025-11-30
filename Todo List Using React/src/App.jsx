cbfd8bc28a6e2ec3ebcd3baaea55b41be66e006c
FazaleAmmar <fazaleammar786@gmail.com>
2025-11-30 16:11:50 +0500

    Line through

--- Todo List Using React/src/App.jsx
+++ Todo List Using React/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,18 +7,34 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const handleAdd = () => {
-    setTodos([...todos, { todo, isCompleted: false }]);
+    if (todo.trim() === "") return;
+
+    setTodos([...todos, { id: uuidv4(), todo: todo, isCompleted: false }]);
     setTodo("");
-    console.log(todos);
   };
 
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
 
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+
+    let index = todos.findIndex((todo) => todo.id === id);
+
+    if (index === -1) return;
+
+    let newTodos = [...todos];
+    newTodos[index] = {
+      ...newTodos[index],
+      isCompleted: !newTodos[index].isCompleted,
+    };
+    setTodos(newTodos);
+  };
+
   return (
     <>
-      <div className="container h-screen w-full bg-[#f7f8fa]">
+      <div className="container min-h-screen w-full bg-[#f7f8fa] flex items-center flex-col">
         <Navbar />
 
         <div className="input w-full h-30 flex items-center justify-center">
@@ -31,19 +48,30 @@ function App() {
           />
           <button
             className="outline-none border-none bg-blue-500 rounded-r-full h-16 w-20 font-bold text-xl text-white hover:bg-blue-700 cursor-pointer active:bg-blue-800"
+            type="button"
             onClick={handleAdd}
-            type="submit"
           >
             Add
           </button>
         </div>
 
-        <div className="todos h-[60vh] w-[85vw] mx-35 rounded-2xl p-5 bg-red-500 overflow-auto flex
-        flex-col items-center gap-5">
+        <div
+          className="todos w-[85vw] min-h-[65vh] rounded-2xl p-5 bg-red-500 flex
+        flex-col items-center gap-5"
+        >
           {todos.map((todo) => (
-            <div className="todo w-1/2 h-25 bg-red-400 rounded-3xl flex items-center justify-between px-2">
-              <div className="text">
-                <input className="mx-2" type="checkbox" />
+            <div
+              key={todo.id}
+              className="todo min-w-116 h-20 bg-red-400 rounded-3xl flex items-center justify-between px-2"
+            >
+              <div className={todo.isCompleted ? "line-through" : ""}>
+                <input
+                  name={todo.id}
+                  checked={todo.isCompleted}
+                  onChange={handleCheckbox}
+                  className="mx-2"
+                  type="checkbox"
+                />
                 {todo.todo}
               </div>
               <div className="btns">
